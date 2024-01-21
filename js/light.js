66b6dfe82d09dc22566e5181749b039ed1c77f21
袁磊（上海） <1452491917@qq.com>
2024-01-21 23:03:53 +0800

    feat: add code

---
+++ js/light.js
@@ -0,0 +1,21 @@
+function sleep(time) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve, time);
+  })
+}
+
+function red() { }
+function yellow() { }
+function green() { }
+
+async function go() {
+  red();
+  await sleep(1000);
+  yellow();
+  await sleep(300);
+  green();
+  await sleep(500);
+  run();
+}
+
+go();
