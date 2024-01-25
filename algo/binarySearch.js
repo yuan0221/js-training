877821c5398246c2ed5c8bf384ef3f047e23a3ff
袁磊（上海） <1452491917@qq.com>
2024-01-25 21:53:52 +0800

    feat: add code

--- algo/binarySearch.js
+++ algo/binarySearch.js
@@ -1,24 +1,41 @@
-// 递归实现二分查找
+// dfs
 const arr = [1, 2, 3, 5, 7, 9, 18, 100];
 const target = 9;
 
-function fn(arr, target, start, end) {
-  start = start >= 0 ? start : 0;
-  end = end >= 0 ? end : arr.length;
+function binarySearchByDFS(arr, target, start = 0, end = arr.length) {
   let midlleIndex = Math.floor((start + end) / 2);
+  
   if (target === arr[midlleIndex]) {
     return midlleIndex;
-  }
+  } 
   else if (target < arr[midlleIndex]) {
-    return fn(arr, target, start, midlleIndex - 1)
-  }
+    return binarySearchByDFS(arr, target, start, midlleIndex - 1);
+  } 
   else if (target > arr[midlleIndex]) {
-    return fn(arr, target, midlleIndex + 1, end)
+    return binarySearchByDFS(arr, target, midlleIndex + 1, end);
   }
   else {
-    return -1
+    return -1;
   }
 }
 
-const r = fn(arr, target);
-console.log(r);
+console.log(binarySearchByDFS(arr, target));
+
+
+// 双指针
+function binarySearchByDoublePointer(arr, target) {
+  let i = 0, j = arr.length - 1; // 初始化双指针
+  
+  // while 循环开启搜索区间，当区间为空时跳出
+  while(i <= j) {
+    let middleIndex = Math.floor((i + j) / 2);
+
+    if(target > arr[middleIndex]) i = middleIndex + 1;
+    else if(target < arr[middleIndex]) j = middleIndex - 1;
+    else return middleIndex;
+  }
+
+  return -1;
+}
+
+console.log(binarySearchByDoublePointer(arr, target));
