20da239123bad229fe77ca56cd971352d48d9629
袁磊（上海） <1452491917@qq.com>
2024-01-26 16:36:26 +0800

    feat: add code

---
+++ algo/sort-bubble.js
@@ -0,0 +1,15 @@
+// 冒泡
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+
+
+console.log(bubbleSort([3, 4, 2, 1, 5, 3, 7, 0, 9]));
