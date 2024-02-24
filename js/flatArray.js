20c378dbe860789a346646ebae7d2c7ac2cf244f
袁磊（上海） <1452491917@qq.com>
2024-02-24 22:33:38 +0800

    feat: add flatArrayByStack

--- js/flatArray.js
+++ js/flatArray.js
@@ -5,14 +5,14 @@ function arrFlatByDfs(rootArr) {
   let res = [];
 
   function dfs(arr) {
-    for(item of arr) {
-      if(Array.isArray(item)) dfs(item);
+    for (item of arr) {
+      if (Array.isArray(item)) dfs(item);
       else res.push(item);
     }
   }
 
   dfs(rootArr);
-  
+
   return res;
 }
 
@@ -23,11 +23,26 @@ console.log(arrFlatByDfs(rootArr));
 // reduce
 function arrFlatByReduce(rootArr) {
   return rootArr.reduce((pre, cur) => {
-    if(Array.isArray(cur)) pre.push(...arrFlatByReduce(cur));
+    if (Array.isArray(cur)) pre.push(...arrFlatByReduce(cur));
     else pre.push(cur);
 
     return pre;
   }, [])
 }
-
 console.log(arrFlatByReduce(rootArr));
+
+
+// stack
+function flatArrayByStack(rootArr) {
+  const stack = rootArr;
+  let res = [];
+
+  while (stack.length) {
+    const item = stack.shift();
+    if (Object.prototype.toString.call(item) === '[object Array]') stack.push(...item);
+    else res.push(item);
+  }
+
+  return res;
+}
+console.log(flatArrayByStack(rootArr));
