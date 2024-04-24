f4ef1c53179b575feae4e29e4db4963376efae0b
袁磊（上海） <1452491917@qq.com>
2024-04-24 20:14:23 +0800

    perf: remove empty property

--- js/removeEmptyProperty.js
+++ js/removeEmptyProperty.js
@@ -1,13 +1,20 @@
 // 问题：移除对象中的空属性
 function removeEmptyProperty(obj) {
-  for([k, v] of Object.entries(obj)) {
-    if(v === null) Reflect.deleteProperty(obj, k);
+  for ([k, v] of Object.entries(obj)) {
+    if (v instanceof Object) {
+      removeEmptyProperty(v)
+    }
+    if (v === null) Reflect.deleteProperty(obj, k);
   }
-} 
+}
 
 let obj = {
   a: 1,
   b: null,
+  c: {
+    d: 2,
+    e: null
+  }
 }
 
 removeEmptyProperty(obj);
