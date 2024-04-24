c5ec3d5edbc7ced6fa01779087b90153eb3065f1
袁磊（上海） <1452491917@qq.com>
2024-04-24 19:51:00 +0800

    feat: 数组去重

--- js/arrayDeduplication.js
+++ js/arrayDeduplication.js
@@ -1,10 +1,26 @@
 // 问题：数组去重
 function deduplication(arr) {
-  return arr.reduce((acc, cur) => {
-    if (!acc.includes(cur)) acc.push(cur);
-    return acc;
+  return arr.reduce((pre, cur) => {
+    if (!pre.includes(cur)) {
+      pre.push(cur)
+    }
+    return pre
   }, [])
 }
-
 const arr = [1, 2, 1, 3, 4];
 console.log(deduplication(arr));
+
+
+
+function deduplication2(arr) {
+  let res = []
+
+  for (item of arr) {
+    if (!res.includes(item)) {
+      res.push(item)
+    }
+  }
+
+  return res
+}
+console.log(deduplication2(arr));
