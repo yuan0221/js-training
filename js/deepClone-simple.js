1a14d635f45d3bbc1f1f51b3476eba1ea3ce3a96
袁磊（上海） <1452491917@qq.com>
2024-04-24 20:04:29 +0800

    perf: 深拷贝

--- js/deepClone-simple.js
+++ js/deepClone-simple.js
@@ -1,44 +1,16 @@
 // 深拷贝
 function deepClone(obj) {
-  let cloneObj;
-  if(Object.prototype.toString.call(obj) === '[object Object]') {
-    cloneObj = {};
-    for(let i in obj) {
-      if(Object.prototype.toString.call(obj[i]) === '[object Object]') {
-        cloneObj = deepClone(obj[i]);
-      } else {
-        cloneObj[i] = obj[i];
-      }
-    }
-  } else if(Object.prototype.toString.call(obj) === '[object Array]') {
-    cloneObj = [];
-    for(let i in obj) {
-      if(Object.prototype.toString.call(obj[i]) === '[object Array]') {
-        cloneObj = deepClone(obj[i]);
-      } else {
-        cloneObj[i] = obj[i];
-      }
-    }
-  } else {
-    cloneObj = obj;
-  }
-  return cloneObj;
-}
+  let cloneObj
 
-function deepClone2(obj) {
-  let cloneObj;
-  if(typeof obj === 'object' && obj !== null) {
-    cloneObj = obj instanceof Object ? {} : [];
-    for(let i in obj) {
-      // if(typeof obj[i] === 'object' && obj[i] !== null) {
-      //   cloneObj[i] = deepClone2(obj[i]);
-      // } else {
-      //   cloneObj[i] = obj[i];
-      // }
-      cloneObj[i] = deepClone2(obj[i]);
+  if (typeof obj === 'object' && obj !== null) {
+    cloneObj = obj instanceof Object ? {} : []
+
+    for (let key in obj) {
+      cloneObj[key] = deepClone(obj[key])
     }
   } else {
-    cloneObj = obj;
+    cloneObj = obj
   }
-  return cloneObj;
+
+  return cloneObj
 }
